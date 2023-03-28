c5cecacc6e5a39093a81e24ffa5cece9d18b1425
Dmitry Ishkov <dmitryishkov27@gmail.com>
2023-03-28 23:44:09 +0300

    Solved Task #2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listIngredientsEl = document.querySelector('#ingredients');
+
+ingredients.map((el, i, arr) => {
+  const itemIngredientsEl = document.createElement('li');
+  itemIngredientsEl.classList.add('item');
+  itemIngredientsEl.textContent = el;
+  arr.splice(i, 1, itemIngredientsEl);
+});
+
+listIngredientsEl.append(...ingredients);
