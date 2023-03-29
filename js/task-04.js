5cb18948447aa7792c21ec2fb6b30d25d053be39
Dmitry Ishkov <dmitryishkov27@gmail.com>
2023-03-29 23:39:32 +0300

    Solved Task #4

---
+++ js/task-04.js
@@ -0,0 +1,12 @@
+const valueEl = document.querySelector('#value');
+const operateBtns = document.querySelectorAll('button[data-action]');
+let counterValue = 0;
+
+operateBtns.forEach(btn => {
+  btn.addEventListener('click', () => {
+    btn.dataset.action.includes('increment')
+      ? (counterValue += 1)
+      : (counterValue -= 1);
+    valueEl.textContent = counterValue;
+  });
+});
