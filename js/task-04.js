79241aec4b724a967a2ba8423f54e92740db7282
Dmitry Ishkov <dmitryishkov27@gmail.com>
2023-03-30 00:23:36 +0300

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -2,7 +2,7 @@ const valueEl = document.querySelector('#value');
 const operateBtnsEl = document.querySelectorAll('button[data-action]');
 let counterValue = 0;
 
-operateBtns.forEach(btn => {
+operateBtnsEl.forEach(btn => {
   btn.addEventListener('click', () => {
     btn.dataset.action.includes('increment')
       ? (counterValue += 1)
