a428f0f3eb287e7797d23f231c898f31f5bc8064
Dmitry Ishkov <dmitryishkov27@gmail.com>
2023-03-30 22:06:38 +0300

    Solved Task #9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,11 @@
+const changeColorBtnEl = document.querySelector('.change-color');
+const colorValueEl = document.querySelector('.color');
+
+changeColorBtnEl.addEventListener('click', () => {
+  colorValueEl.textContent = getRandomHexColor();
+  document.body.style.backgroundColor = colorValueEl.textContent;
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
