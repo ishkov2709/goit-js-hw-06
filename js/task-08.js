b6a4e4a1c7fd5610cb18c37f58b82f42de3a9331
Dmitry Ishkov <dmitryishkov27@gmail.com>
2023-03-30 21:41:37 +0300

    Solved Task #8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const formSendEl = document.querySelector('.login-form');
 
+formSendEl.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+  const filledForm = { email: email.value, password: password.value };
+
+  if (email.value === '' || password.value === '') {
+    return alert('Please fill in all the fields!');
+  }
+
+  console.log(filledForm);
+  event.currentTarget.reset();
+});
